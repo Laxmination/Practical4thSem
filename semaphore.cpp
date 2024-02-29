b4142c38ef9d656cb2df957746480a1e18393eea
laxmination <laxmikathayat52@gmail.com>
2024-02-29 23:01:23 +0545

    asdfsd

--- semaphore.cpp
+++ semaphore.cpp
@@ -1,22 +1,47 @@
 #include<iostream>
+#include<mutex>
+#include<thread>
+#include<condition_variable>
 
 #define N 100
-typedef semaphore;
-semaphore =1; /*control access to CR*/
-semaphore empty =N;
-semaphore full =0;
+
+std::mutex mutex;
+std::condition_variable full, empty;
 
 void procedure(void)
 {
-    int item;
-    while (TRUE)
-    {
-        item =produce.item();
-        /*Generate something to put in buffer*/
-        down(empty); /*decrement empty count*/
-        down(&mute x) /*enter CR*/
+int item;
+while (true)
+     {
+         item =produce.item();
+         empty.wait(); 
+         std: :unique_lock<std::mutex> lock(mutex);
 
-        insert_item(item); /*put*/
+        insert_item(item);
+        lock.unlock();
+        full.notify_one();
+     }
+     }
+
+void consumer(void)
+{
+    int item;
+    while (true)
+    {
+       std::unique_lock<std::mutex> lock(mutex); 
+       full.wait(lock); 
+       item = remove_item(); 
+       lock.unlock();  
+       empty.notify_one();
+       consume_item(item);
     }
-    
+}
+
+int main()
+{
+    std::thread producer_thread(procedure);
+    std::thread consumer_thread(consumer);
+    producer_thread.join();
+    consumer_thread.join();
+    return 0;
 }
